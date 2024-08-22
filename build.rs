b515d11b55f70787902eff9652b1e7dd050c6367
blookewoost <blakewest1005@gmail.com>
2024-08-21 22:03:31 -0400

    adding a couple comments to clear things up

--- build.rs
+++ build.rs
@@ -1,11 +1,20 @@
 fn main() {
 
+    /*
+    
+    Communicate with cargo via stdout using println!
+        - tell cargo where to look for the library we want to link
+        - tell cargo which library we want to link
+        - generate bindings to a file
+
+    */
+    
     println!("cargo:rustc-link-search=/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-lib=m");
+    println!("cargo:rustc-link-lib=m"); // m is libm.so, the 'lib' prefix and the extension are assumed.
 
     let bindings = bindgen::Builder::default()
         .header("/usr/include/math.h")
-        .allowlist_function("sqrt")
+        .allowlist_function("sqrt") // let's not generate everything. Whitelist the sqrt function
         .generate()
         .expect("Unable to generate bindings");
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 pub mod bindings;
 
+/*
+
+A simple file to showcase using bindgen for Rust/C interop.
+
+*/
+
 fn main() {
     // Perform a mathematical operation through the C FFI to libm
     let x: f64 = 4.0;
